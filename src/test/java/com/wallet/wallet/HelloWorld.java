47024616649b0ad862331f3cfd67309a8d29727c
Alexandre Ferri <alexandre.o.ferri@gmail.com>
2020-09-07 17:20:58 -0300

    Initial configuration for TDD tests

--- src/test/java/com/wallet/wallet/HelloWorld.java
+++ src/test/java/com/wallet/wallet/HelloWorld.java
@@ -3,16 +3,17 @@ package com.wallet.wallet;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles("test")
-public class WalletApplicationTests {
+public class HelloWorld {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void testHelloWorld() {
+        assertEquals(1, 1);
+    }
 
 }
